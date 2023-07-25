ac55b098578ee47fa76d8e4e73f86292dd90f946
destineevaldivia <destvaldivia@gmail.com>
2023-07-25 16:06:20 -0700

    linked js to html. Centered heading and description using JS

---
+++ RecipePage/RecipePage.js
@@ -0,0 +1,9 @@
+//centered the heading using JS
+
+let headingEl = document.querySelector("#heading");
+heading.style.textAlign = "center";
+
+//centered the description using JS
+let descriptionEl = document.querySelector("#description");
+descriptionEl.style.textAlign = "center";
+
