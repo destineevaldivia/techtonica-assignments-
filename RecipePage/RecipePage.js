c87c8a8de1ce11af19bbdb58359b416f718e32e7
destineevaldivia <destvaldivia@gmail.com>
2023-07-27 13:57:49 -0700

    added dic container to html

--- RecipePage/RecipePage.js
+++ RecipePage/RecipePage.js
@@ -11,5 +11,5 @@ let otherRecipesEls = document.createElement("img")
     otherRecipesEls.src = "http://www.cannellevanille.com/wp-content/uploads/2013/07/Frittata_AranGoyoaga_edoc2784.jpg";
     otherRecipesEls.alt = "Photo of Ricotta Fritatta";
 
-    document.body.appendChild(otherRecipesEls);
+    document.body.appendChild("MoreRecipesTitle");
     
