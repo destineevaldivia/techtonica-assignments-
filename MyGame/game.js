c843e67e7c574d14e1f547a957d8653fe81bb9d9
destineevaldivia <destvaldivia@gmail.com>
2023-08-11 09:02:34 -0700

    finished JS outline for my game

---
+++ MyGame/game.js
@@ -0,0 +1,21 @@
+//grab all h2
+let computerChoice = document.getElementById('computer-choice');
+let yourChoice = document.getElementById('your-choice');
+let resultDisplay = document.getElementById('result');
+//grab all choices
+let possibleChoices = document.getElementsByClassName('button');
+
+//add event listener
+possibleChoices.forEach(possibleChoices.addEventListener('click', (event) => {
+    //yourChoice = display your choice
+    //generate computer choice
+}))
+
+function generateComputerChoice() {
+    // let randomNumber = Math.floor(Math.random() * 3)//or you can use possible choices
+}
+
+
+ //results function
+
+ //keep score function best of 3
