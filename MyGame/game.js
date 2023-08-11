651bcd76a665544102c8ee74f9373c0853afe843
destineevaldivia <destvaldivia@gmail.com>
2023-08-11 09:21:45 -0700

    added styling: background color and font

--- MyGame/game.js
+++ MyGame/game.js
@@ -1,15 +1,36 @@
-//grab all h2
-let computerChoice = document.getElementById('computer-choice');
-let yourChoice = document.getElementById('your-choice');
-let resultDisplay = document.getElementById('result');
-//grab all choices
+//grab all Displays
+let displayComputerChoice = document.getElementById('computer-choice');
+let displayYourChoice = document.getElementById('your-choice');
+let displayResults = document.getElementById('result');
+//grab all possible choices
 let possibleChoices = document.getElementsByClassName('button');
+// grab each button choice
+let rockBtn = document.getElementById('rock');
+let paperBtn = document.getElementById('paper');
+let scissorsBtn = document.getElementById('scissors');
 
-//add event listener
-possibleChoices.forEach(possibleChoices.addEventListener('click', (event) => {
+//add event listeners
+rockBtn.addEventListener("click", (event) => {
+    
+});
+
+paperBtn.addEventListener("click", (event) => {
+
+});
+
+scissorsBtn.addEventListener("click", (event) => {
+
+});
+
+
+
+
+//possibleChoices.addEventListener("click", (event) => {
+    
+    
     //yourChoice = display your choice
     //generate computer choice
-}))
+// })
 
 function generateComputerChoice() {
     // let randomNumber = Math.floor(Math.random() * 3)//or you can use possible choices
